2cc6dbb99ca82227899266354707aafd0d085b19
Md Johirul Islam <johirbuet@gmail.com>
2016-12-27 12:21:26 -0600

    Defination of Builder Pattern

--- src/com/design/pattern/creational/builder/BuilderPatternDemo.java
+++ src/com/design/pattern/creational/builder/BuilderPatternDemo.java
@@ -1,5 +1,15 @@
 package com.design.pattern.creational.builder;
 
+/**
+ * Builder pattern builds a complex object using simple objects and using a step by step approach. 
+ * This type of design pattern comes under creational pattern as this pattern provides one of the 
+ * best ways to create an object.
+ * A Builder class builds the final object step by step. This builder is independent of other objects.
+
+
+ * @author mislam
+ *
+ */
 public class BuilderPatternDemo {
 	   public static void main(String[] args) {
 	   
